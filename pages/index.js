96393b2c73fd326a6fb566da3e137ab56463219d
mendes-lad <lucas.mendes@synvia.com>
2024-08-13 14:41:03 -0300

    teste dominio

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function home() {
-    return <h1>Treinamento git, frase alterada para treinamento</h1>
+    return <h1>Treinamento git, frase alterada para treinamento.
+        Adicionada mais uma linha
+    </h1>
 }
 
 export default home;
